0743791174e62cfeb8cb9a380be03c566a2888fb
jojomak13 <jojomak350@gmail.com>
2021-03-30 12:06:01 +0200

    make updates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-export * from './core/Factory';
+import faker from 'faker';
+import { Model } from 'mongoose';
+
+class Factory {
+  private blueprint: Function;
+  private model: Model<any, any>;
+
+  constructor(model: Model<any, any>, blueprint: Function) {
+    this.model = model;
+    this.blueprint = blueprint;
+  }
+
+  public static define(model: Model<any, any>, blueprint: Function) {
+    const factory = new this(model, blueprint);
+
+    return factory;
+  }
+
+  /**
+   *
+   * @param count
+   * @returns Promise<boolean>
+   */
+  public async create(count: number = 1): Promise<boolean> {
+    for (let i = 0; i < count; i++) {
+      await this.model.create(this.blueprint(faker));
+    }
+
+    return true;
+  }
+}
+
+export { Factory };
